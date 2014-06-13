0d1dcacf455597f004ecf9af51fae08d3e8f581d
NomNuggetNom <nomnuggetnom@gmail.com>
2014-06-13 12:15:36 -0400

    Change pattern

--- assets/javascripts/mcheads.js
+++ assets/javascripts/mcheads.js
@@ -1,9 +1,13 @@
-function details(text) {
-  if (text.indexOf("[head:") > -1); {
-    var adjusted = "[img]http://cravatar.eu/helmavatar/"
-    + text.replace(/\[head:([a-zA-Z0-9]+)]/, "$1")
-    + "/16.png[/img]";
-  }
+function mcheads(text) {
+  //Expression to use
+  var re = /\[head:([a-zA-Z0-9]+)]/;
+  
+  // Adjusted text
+  var adjusted = 
+  "[img]http://cravatar.eu/helmavatar/"
+  + srctext.replace(re, "$1")
+  + "/16.png[/img]";
+
  return adjusted;
 }
 
@@ -11,7 +15,7 @@ Discourse.Dialect.postProcessText(function (text) {
   text = [].concat(text);
   for (var i = 0; i < text.length; i++) {
     if (text[i].length > 0 && text[i][0] !== "<") {
-      text[i] = details(text[i]);
+      text[i] = mcheads(text[i]);
     }
   }
   return text;

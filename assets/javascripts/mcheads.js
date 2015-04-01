44db76c621a164fcb4a453068aa10d1365d656c9
NomNuggetNom <nomnuggetnom@gmail.com>
2015-03-31 22:49:36 -0400

    Update mcheads.js

--- assets/javascripts/mcheads.js
+++ assets/javascripts/mcheads.js
@@ -4,6 +4,7 @@
     matcher: /^\[head:([a-zA-Z0-9_]{2,16})\]/,
     emitter: function(contents) {
       var username = contents[1];
+      console.log(username);
       return ['img', {src: ['http://cravatar.eu/avatar/' + username + '/16.png']} ];
     }
   });
